1f638732adfa462261b8cce5063d6744abe26c83
Joffrey-Liu <32201219+Joffrey-Liu@users.noreply.github.com>
2017-09-22 23:46:53 +0800

    Upload C# version

---
+++ source/視窗HW6/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace 視窗HW6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(572, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(53, 20);
+            this.toolStripMenuItem1.Text = "Game";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem2.Text = "Restart";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 272);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(572, 69);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Remaining:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(102, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Received";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(102, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 341);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ source/視窗HW6/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace 視窗HW6
+{
+    public partial class Form1 : Form
+    {
+        Image banana = Properties.Resources.Banana;
+        Image bowl = Properties.Resources.Bowl;
+        Image strawberry = Properties.Resources.StawBerry;
+        Image tomato = Properties.Resources.Tomato;
+        Image egg1 = Properties.Resources.C0iEi37;
+        Image egg2 = Properties.Resources.XT5Bh4j;
+        Image egg1thumb;
+        Image egg2thumb;
+        Image[] fruit = new Image[3];
+        int a = 120;
+        int timeforbackpicture = 0;
+        int numforbackpicture = 0;
+        Random rd = new Random();
+        int x1, y1, speed1;
+        int x2, y2;
+        int x3, y3;
+        int mouseposx;
+        int catchnum;
+        int x, y;
+        public Form1()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            egg1thumb = egg1.GetThumbnailImage(500, (int)(500 * egg1.Height / egg1.Width), null, (IntPtr)0);
+            egg2thumb = egg2.GetThumbnailImage(500, (int)(500 * egg1.Height / egg1.Width), null, (IntPtr)0);
+            if(egg1thumb != null && egg2thumb != null)
+            {
+                float[][] cmarray = 
+                {
+                  new float[] {1, 0, 0, 0,    0},
+                  new float[] {0, 1, 0, 0,    0},
+                  new float[] {0, 0, 1, 0,    0},
+                  new float[] {0, 0, 0, 0.3f, 0},
+                  new float[] {0, 0, 0, 0,    1}
+
+                };
+                ColorMatrix cm = new ColorMatrix(cmarray);
+                ImageAttributes ia = new ImageAttributes();
+                ia.SetColorMatrix(cm, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
+                if (numforbackpicture == 1)
+                {
+                    Rectangle recdest = new Rectangle(0, 0, egg1thumb.Width, egg1thumb.Height);
+                    e.Graphics.DrawImage(egg1thumb, recdest, 0, 0, egg1thumb.Width, egg1thumb.Height, GraphicsUnit.Pixel, ia);
+                }
+                if (numforbackpicture == 2)
+                {
+                    Rectangle recdest = new Rectangle(0, 0, egg2thumb.Width, egg2thumb.Height);
+                    e.Graphics.DrawImage(egg2thumb, recdest, 0, 0, egg2thumb.Width, egg2thumb.Height, GraphicsUnit.Pixel, ia);
+                }
+
+                Rectangle bananadest = new Rectangle(x1, y1, banana.Width, banana.Height);
+                e.Graphics.DrawImage(banana, bananadest);
+
+                Rectangle strawberrydest = new Rectangle(x2, y2, strawberry.Width, strawberry.Height);
+                e.Graphics.DrawImage(strawberry, strawberrydest);
+
+                Rectangle tomatodest = new Rectangle(x3, y3, tomato.Width, tomato.Height);
+                e.Graphics.DrawImage(tomato, tomatodest);
+
+                Rectangle bowldest = new Rectangle(mouseposx, 280, bowl.Width, bowl.Height);
+                e.Graphics.DrawImage(bowl, bowldest);
+
+               
+             
+            }
+            
+           
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (timeforbackpicture % 10 == 0)
+            {
+                if (numforbackpicture == 2)
+                    numforbackpicture = 0;
+                numforbackpicture++;
+                this.Invalidate();
+            }
+            timeforbackpicture++;  
+            a--;
+            label2.Text = a + "seconds";
+            
+            
+            
+            if (a == 0)//所有的時間都要暫停
+            {
+                timer1.Stop();
+                timer2.Stop();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 1000;
+            timer2.Interval = 500;
+            timer1.Start();
+            timer2.Start();
+            label2.Text = a + "seconds";
+            this.ClientSize = new Size(500, 400);
+            catchnum = 0;
+            speed1 = 30;
+
+            x1 = rd.Next(0, 450);
+            y1 = rd.Next(1, 20);
+            y1 = -y1;
+            x2 = rd.Next(0,450);
+            y2 = rd.Next(1,100);
+            y2 = -y2;
+            x3 = rd.Next(0, 450);
+            y3 = rd.Next(1, 100);
+            y3 = -y3;
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            a = 120;
+            timeforbackpicture = 0;
+            numforbackpicture = 0;
+            x1 = rd.Next(0, 400);
+            y1 = rd.Next(1, 20);
+            y1 = -y1;
+            x2 = rd.Next(0, 400);
+            y2 = rd.Next(1, 100);
+            y2 = -y2;
+            x3 = rd.Next(0, 400);
+            y3 = rd.Next(1, 100);
+            y3 = -y3;
+            timer1.Start();
+            timer2.Start();
+            catchnum = 0;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            fruit[0] = banana;
+            fruit[1] = strawberry;
+            fruit[2] = tomato;
+
+            y1 += speed1;
+            y2 += speed1;
+            y3 += speed1;
+            if (x1 >= mouseposx && (x1 + 24) <= (mouseposx + 70) && (y1 + 35) >= 280 && y1 < 280)
+            {
+                y1 += 500;
+                catchnum++;
+            }
+
+            if (x2 >= mouseposx && (x2 + 38) <= (mouseposx + 70) && (y2 + 40) >= 280 && y2 < 280)
+            {
+                y2 += 500;
+                catchnum++;
+            }
+
+            if (x3 >= mouseposx && (x3 + 38) <= (mouseposx + 70) && (y3 + 40) >= 280 && y3<280)
+            {
+                y3 += 500;
+                catchnum++;
+            }
+            if (y1 >= 280 && y2 >= 280 && y3 >= 280)
+            {
+                x1 = rd.Next(0, 400);
+                y1 = rd.Next(1, 20);
+                y1 = -y1;
+                x2 = rd.Next(0, 400);
+                y2 = rd.Next(1, 100);
+                y2 = -y2;
+                x3 = rd.Next(0, 400);
+                y3 = rd.Next(1, 100);
+                y3 = -y3;
+ 
+            }
+            label4.Text = catchnum.ToString();
+            this.Invalidate();
+            
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            mouseposx = e.X;
+            if ((e.X + 70) >= 500)
+                mouseposx = 430;
+            this.Invalidate();
+        }
+    }
+}
